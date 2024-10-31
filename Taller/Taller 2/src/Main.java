c7a43542b20d00596fc48773ed99eb501dc2e438
Pascal Ordano <pascalordanodev@gmail.com>
2024-10-30 23:57:50 -0300

    Changes2

--- Taller/Taller 2/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[] Args){
-        A an = new A();
-        System.out.println(an.m1());
-    }
-
-}
--- Teoricas/14-8/DateTester.java
+++
@@ -1,7 +0,0 @@
-
-class DateTester {
-    public static void main(String[] args){
-        Date d1 = new Date(10, 12, 2020);
-        System.out.println(d1.getDay());
-    }
-}
